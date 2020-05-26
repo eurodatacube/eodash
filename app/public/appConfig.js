a5760bd431c8b29be0987fa9e282fef97e28713a
paris <silvester.pari@eox.at>
2020-05-26 10:34:27 +0200

    changed hedaer title

--- app/public/appConfig.js
+++ app/public/appConfig.js
@@ -2,7 +2,7 @@
 const appConfig = (() => {
   return {
     branding: {
-      appName: 'Rapid Action on Covid-19 and EO',
+      appName: 'Rapid Action on coronavirus and EO',
       primaryColor: '#003247',
       headerLogo: '/eodash-data/general/ESA_Logo.svg',
     },
