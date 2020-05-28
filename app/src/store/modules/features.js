3491c9247433a3b5418613a0d9012f498040d0cf
Stephan Meißl <stephan.meissl@eox.at>
2020-05-28 22:20:22 +0200

    Only consider csvs that have at least the AOI column. Use AOI_ID for dummies.

--- app/src/store/modules/features.js
+++ app/src/store/modules/features.js
@@ -113,93 +113,95 @@ const actions = {
       skipEmptyLines: true,
       delimiter: ',',
       complete: (results) => {
-        commit('SET_RESULTS_COUNT', results.data.length);
-        const wkt = new Wkt();
-        // Sort results by time
-        results.data.sort((a, b) => moment.utc(a.Time).diff(moment.utc(b.Time)));
-        /* Aggregate data based on AOI and indicator type */
-        const featureObjs = {};
-        for (let rr = 0; rr < results.data.length; rr += 1) {
-          const uniqueKey = `${results.data[rr].AOI}_${results.data[rr]['Indicator code']}`;
-          if (Object.prototype.hasOwnProperty.call(featureObjs, uniqueKey)) {
-            featureObjs[uniqueKey]['Indicator Value'].push(
-              results.data[rr]['Indicator Value'],
-            );
-            const measurement = results.data[rr]['Measurement Value'].replace(',', '.');
-            featureObjs[uniqueKey]['Measurement Value'].push(
-              measurement.length !== 0 ? Number(measurement) : NaN,
-            );
-            featureObjs[uniqueKey]['Reference value'].push(
-              results.data[rr]['Reference value'],
-            );
-            featureObjs[uniqueKey]['Reference time'].push(
-              results.data[rr]['Reference time'],
-            );
-            featureObjs[uniqueKey].Time.push(
-              moment.utc(results.data[rr].Time).toDate(),
-            );
-            featureObjs[uniqueKey]['Color code'].push(
-              results.data[rr]['Color code'],
-            );
-          } else {
-            featureObjs[uniqueKey] = results.data[rr];
-            featureObjs[uniqueKey]['Indicator Value'] = [
-              featureObjs[uniqueKey]['Indicator Value'],
-            ];
-            featureObjs[uniqueKey]['Color code'] = [
-              featureObjs[uniqueKey]['Color code'],
-            ];
-            const measurement = featureObjs[uniqueKey]['Measurement Value'].replace(',', '.');
-            featureObjs[uniqueKey]['Measurement Value'] = [
-              measurement.length !== 0 ? Number(measurement) : NaN,
-            ];
-            featureObjs[uniqueKey]['Reference value'] = [
-              featureObjs[uniqueKey]['Reference value'],
-            ];
-            featureObjs[uniqueKey]['Reference time'] = [
-              featureObjs[uniqueKey]['Reference time'],
-            ];
-            // dummy empty geometry
-            let ftrs = [];
-            try {
-              // assuming sub-aoi does not change over time
-              wkt.read(featureObjs[uniqueKey]['Sub-AOI']);
-              const jsonGeom = wkt.toJson();
-              // create a feature collection
-              ftrs = [{
-                type: 'Feature',
-                properties: {},
-                geometry: jsonGeom,
-              }];
-            } catch (err) {} // eslint-disable-line no-empty
-            const ftrCol = {
-              type: 'FeatureCollection',
-              features: ftrs,
-            };
-            featureObjs[uniqueKey]['Sub-AOI'] = ftrCol;
-            featureObjs[uniqueKey].Time = [
-              moment.utc(featureObjs[uniqueKey].Time).toDate(),
-            ];
+        if (results.data[0].AOI) {  // only continue if AOI column is present
+          commit('SET_RESULTS_COUNT', results.data.length);
+          const wkt = new Wkt();
+          // Sort results by time
+          results.data.sort((a, b) => moment.utc(a.Time).diff(moment.utc(b.Time)));
+          /* Aggregate data based on AOI and indicator type */
+          const featureObjs = {};
+          for (let rr = 0; rr < results.data.length; rr += 1) {
+            const uniqueKey = `${results.data[rr].AOI}_${results.data[rr]['Indicator code']}`;
+            if (Object.prototype.hasOwnProperty.call(featureObjs, uniqueKey)) {
+              featureObjs[uniqueKey]['Indicator Value'].push(
+                results.data[rr]['Indicator Value'],
+              );
+              const measurement = results.data[rr]['Measurement Value'].replace(',', '.');
+              featureObjs[uniqueKey]['Measurement Value'].push(
+                measurement.length !== 0 ? Number(measurement) : NaN,
+              );
+              featureObjs[uniqueKey]['Reference value'].push(
+                results.data[rr]['Reference value'],
+              );
+              featureObjs[uniqueKey]['Reference time'].push(
+                results.data[rr]['Reference time'],
+              );
+              featureObjs[uniqueKey].Time.push(
+                moment.utc(results.data[rr].Time).toDate(),
+              );
+              featureObjs[uniqueKey]['Color code'].push(
+                results.data[rr]['Color code'],
+              );
+            } else {
+              featureObjs[uniqueKey] = results.data[rr];
+              featureObjs[uniqueKey]['Indicator Value'] = [
+                featureObjs[uniqueKey]['Indicator Value'],
+              ];
+              featureObjs[uniqueKey]['Color code'] = [
+                featureObjs[uniqueKey]['Color code'],
+              ];
+              const measurement = featureObjs[uniqueKey]['Measurement Value'].replace(',', '.');
+              featureObjs[uniqueKey]['Measurement Value'] = [
+                measurement.length !== 0 ? Number(measurement) : NaN,
+              ];
+              featureObjs[uniqueKey]['Reference value'] = [
+                featureObjs[uniqueKey]['Reference value'],
+              ];
+              featureObjs[uniqueKey]['Reference time'] = [
+                featureObjs[uniqueKey]['Reference time'],
+              ];
+              // dummy empty geometry
+              let ftrs = [];
+              try {
+                // assuming sub-aoi does not change over time
+                wkt.read(featureObjs[uniqueKey]['Sub-AOI']);
+                const jsonGeom = wkt.toJson();
+                // create a feature collection
+                ftrs = [{
+                  type: 'Feature',
+                  properties: {},
+                  geometry: jsonGeom,
+                }];
+              } catch (err) {} // eslint-disable-line no-empty
+              const ftrCol = {
+                type: 'FeatureCollection',
+                features: ftrs,
+              };
+              featureObjs[uniqueKey]['Sub-AOI'] = ftrCol;
+              featureObjs[uniqueKey].Time = [
+                moment.utc(featureObjs[uniqueKey].Time).toDate(),
+              ];
+            }
           }
-        }
-        const features = [];
-        const keys = Object.keys(featureObjs);
+          const features = [];
+          const keys = Object.keys(featureObjs);
 
-        for (let kk = 0; kk < keys.length; kk += 1) {
-          const coordinates = keys[kk].split('_')[0].split(',').map(Number);
-          // console.log(featureObjs[keys[kk]]);
-          featureObjs[keys[kk]].AOI = latLng(coordinates);
-          featureObjs[keys[kk]].id = globalIdCounter; // to connect indicator & feature
-          features.push({
-            latlng: latLng(coordinates),
-            id: globalIdCounter,
-            properties: {
-              indicatorObject: featureObjs[keys[kk]],
-            },
-          });
-          globalIdCounter += 1;
+          for (let kk = 0; kk < keys.length; kk += 1) {
+            const coordinates = keys[kk].split('_')[0].split(',').map(Number);
+            // console.log(featureObjs[keys[kk]]);
+            featureObjs[keys[kk]].AOI = latLng(coordinates);
+            featureObjs[keys[kk]].id = globalIdCounter; // to connect indicator & feature
+            features.push({
+              latlng: latLng(coordinates),
+              id: globalIdCounter,
+              properties: {
+                indicatorObject: featureObjs[keys[kk]],
+              },
+            });
+            globalIdCounter += 1;
+          }
+          commit('ADD_NEW_FEATURES', features);
         }
-        commit('ADD_NEW_FEATURES', features);
       },
     });
   },
@@ -211,28 +213,30 @@ const actions = {
       skipEmptyLines: true,
       delimiter: ',',
       complete: (results) => {
-        const featureObjs = {};
-        for (let rr = 0; rr < results.data.length; rr += 1) {
-          const uniqueKey = `${results.data[rr].AOI}_d}`;
-          featureObjs[uniqueKey] = results.data[rr];
-          featureObjs[uniqueKey]['Indicator code'] = 'd';
-        }
-        const features = [];
-        const keys = Object.keys(featureObjs);
+        if (results.data[0].AOI) {  // only continue if AOI column is present
+          const featureObjs = {};
+          for (let rr = 0; rr < results.data.length; rr += 1) {
+            const uniqueKey = `${results.data[rr].AOI_ID}`;
+            featureObjs[uniqueKey] = results.data[rr];
+            featureObjs[uniqueKey]['Indicator code'] = 'd';
+          }
+          const features = [];
+          const keys = Object.keys(featureObjs);
 
-        for (let kk = 0; kk < keys.length; kk += 1) {
-          const coordinates = keys[kk].split('_')[0].split(',').map(Number);
-          featureObjs[keys[kk]].id = globalIdCounter; // to connect indicator & feature
-          features.push({
-            latlng: latLng(coordinates),
-            id: globalIdCounter,
-            properties: {
-              indicatorObject: featureObjs[keys[kk]],
-            },
-          });
-          globalIdCounter += 1;
+          for (let kk = 0; kk < keys.length; kk += 1) {
+            const coordinates = keys[kk].split('_')[0].split(',').map(Number);
+            featureObjs[keys[kk]].id = globalIdCounter; // to connect indicator & feature
+            features.push({
+              latlng: latLng(coordinates),
+              id: globalIdCounter,
+              properties: {
+                indicatorObject: featureObjs[keys[kk]],
+              },
+            });
+            globalIdCounter += 1;
+          }
+          commit('SET_ALL_DUMMY_LOCATIONS', features);
         }
-        commit('SET_ALL_DUMMY_LOCATIONS', features);
       },
     });
   },
