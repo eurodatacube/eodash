2904e85ea2634892d5eef113a8203998f6822c37
paris <silvester.pari@eox.at>
2020-05-28 11:28:54 +0200

    only display dummy locations if true in config

--- app/src/appConfig.js
+++ app/src/appConfig.js
@@ -14,6 +14,7 @@ const brands = [
       shortDescription: 'The RACE platform demonstrates how the use of EO data can help shed new light on societal and economic changes currently taking place owing to the coronavirus pandemic.',
       twitterCardImagePath: '/img/ESA/RACE_key_visual_200526_3v.jpg',
     },
+    displayDummyLocations: true,
   },
   {
     id: 'trilateral',
