3cb235dcd79ccd1d58ed3ad1ec541979e851ebaa
Lubomir Bucek <lubomir.bucek@eox.at>
2020-05-29 16:01:02 +0200

    add no2 glob date, update some country=all references

--- app/src/components/DataPanel.vue
+++ app/src/components/DataPanel.vue
@@ -184,7 +184,7 @@ export default {
       return ['all', 'regional'].includes(this.indicatorObject.Country);
     },
     countryItemsCount() {
-      const countries = this.getCountries.filter((item) => item !== 'all');
+      const countries = this.getCountries.filter((item) => !['all', 'regional'].includes(item));
       return countries.length;
     },
   },
