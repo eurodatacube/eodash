52d9946e0fa12302ecfa39c936c8793b7f93de92
Stephan Meißl <stephan.meissl@eox.at>
2020-06-23 16:52:16 +0200

    adding Nighlights intro to N5 stories

--- app/src/config/trilateral.js
+++ app/src/config/trilateral.js
@@ -151,7 +151,7 @@ export const indicatorsDefinition = Object.freeze({
   N5: {
     indicator: 'Nightlights (Suomi NPP VIIRS)',
     class: 'economic',
-    story: '/eodash-data/stories/N5',
+    story: '/data/trilateral/N5',
     largeSubAoi: true,
   },
   d: { // dummy for locations
