54e0d32cdceba3c99193c03dc34c10ca5d00238a
Stephan Meißl <stephan.meissl@eox.at>
2020-06-24 21:26:44 +0200

    update short desc

--- app/src/appConfig.js
+++ app/src/appConfig.js
@@ -56,7 +56,7 @@ module.exports = [
     pageMeta: {
       rootPath: 'https://eodashboard.org',
       googleSiteVerification: 'iHN2SaSR9qF7T3lOqNYSF4kaq_ZgK-I31SSgO5RKXW8',
-      shortDescription: 'The Earth Observing Dashboard platform demonstrates how the use of EO data can help shed new light on societal and economic changes currently taking place owing to the coronavirus pandemic.',
+      shortDescription: 'The Earth Observing Dashboard combines the resources, technical knowledge and expertise of three partner agencies ESA, JAXA, and NASA to strengthen our global understanding of the environmental and economic effects of the COVID-19 pandemic.',
       twitterCardImagePath: '/img/trilateral/twitter_card.png',
     },
     displayDummyLocations: './data/trilateral/dummylocations.csv',
