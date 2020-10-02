1cb3512467b74dc08d9486910bfc8781a50e8aab
paris <silvester.pari@eox.at>
2020-10-02 10:18:14 +0200

    move map setup into onmapready function

--- app/src/components/IndicatorMap.vue
+++ app/src/components/IndicatorMap.vue
@@ -412,7 +412,19 @@ export default {
     this.dataLayerIndex = this.usedTimes.length - 1;
     this.dataLayerTime = { value: this.usedTimes[this.dataLayerIndex] };
     this.compareLayerTime = { value: this.getInitialCompareTime() };
-    this.$nextTick(() => {
+  },
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+    boundsUpdated(bounds) {
+      this.bounds = bounds;
+    },
+    onMapReady() {
+      this.map = this.$refs.map.mapObject;
       const layerButtons = document.querySelectorAll('.leaflet-control-layers-toggle');
       layerButtons.forEach((lB) => lB.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${this.appConfig.branding.primaryColor}" width="32px" height="32px"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.99 18.54l-7.37-5.73L3 14.07l9 7 9-7-1.63-1.27-7.38 5.74zM12 16l7.36-5.73L21 9l-9-7-9 7 1.63 1.27L12 16z"/></svg>`); // eslint-disable-line
 
@@ -459,20 +471,6 @@ export default {
       setTimeout(() => {
         this.flyToBounds();
       }, 100);
-    });
-  },
-  methods: {
-    zoomUpdated(zoom) {
-      this.zoom = zoom;
-    },
-    centerUpdated(center) {
-      this.center = center;
-    },
-    boundsUpdated(bounds) {
-      this.bounds = bounds;
-    },
-    onMapReady() {
-      this.map = this.$refs.map.mapObject;
     },
     onResize() {
       // to fix panel size for reference image window
