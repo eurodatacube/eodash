d1d15d0729fb5466b3fa7896621e39ef2bac610d
paris <silvester.pari@eox.at>
2020-10-12 16:30:17 +0200

    [experimental] add dark mode toggle

--- app/src/main.js
+++ app/src/main.js
@@ -109,6 +109,20 @@ const renderVue = async () => {
           warning: '#FFC107',
           grey: '#AAA',
         },
+        dark: {
+          primary: store.state.config.appConfig
+            ? store.state.config.appConfig.branding.primaryColor
+            : '#004170',
+          secondary: store.state.config.appConfig
+            ? store.state.config.appConfig.branding.secondaryColor
+            : '#424242',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+          grey: '#AAA',
+        },
       },
     },
   });
