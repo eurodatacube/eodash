12d4c23d27a3797898f35b6d5d0751c53357b227
aapopescu <35488732+aapopescu@users.noreply.github.com>
2021-02-15 17:02:20 +0100

    Update trilateral.js
    updated GEOGLAM to 2021-01-28

--- app/src/config/trilateral.js
+++ app/src/config/trilateral.js
@@ -2354,7 +2354,7 @@ export const globalIndicators = [
         lastColorCode: null,
         aoi: null,
         aoiID: 'W6',
-        time: getMonthlyDates('2020-01-28', '2020-11-28'),
+        time: getMonthlyDates('2020-01-28', '2021-01-28'),
         inputData: [''],
         display: {
           protocol: 'xyz',
