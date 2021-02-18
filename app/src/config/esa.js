6331dce37a229943924ef4f5a20898cb748f300a
Lubomir Bucek <lubomir.bucek@eox.at>
2021-02-18 10:04:10 +0100

    update cmems config, readding baltic, update names

--- app/src/config/esa.js
+++ app/src/config/esa.js
@@ -1279,18 +1279,24 @@ export const globalIndicators = [
         display: [{
           ...cmemsDisplay,
           baseUrl: 'https://nrt.cmems-du.eu/thredds/wms/dataset-oc-atl-bio-multi-l4-chl_interpolated_1km_daily-rt?COLORSCALERANGE=0.03%2C30&STYLES=boxfill%2Frainbow',
-          name: 'Atlantic coast CHL',
+          name: 'Atlantic coast CHL L4 Product',
           bounds: latLngBounds(latLng([20, -45]), latLng([66, 10.5])),
         }, {
           ...cmemsDisplay,
           baseUrl: 'https://nrt.cmems-du.eu/thredds/wms/dataset-oc-med-chl-multi-l4-interp_1km_daily-rt-v02?COLORSCALERANGE=0.03%2C30&STYLES=boxfill%2Frainbow',
-          name: 'Mediterranean CHL',
+          name: 'Mediterranean CHL L4 Product',
           bounds: latLngBounds(latLng([30, -6]), latLng([46, 37])),
         }, {
           ...cmemsDisplay,
           baseUrl: 'https://nrt.cmems-du.eu/thredds/wms/dataset-oc-bs-chl-multi-l4-interp_1km_daily-rt-v02?COLORSCALERANGE=0.03%2C30&STYLES=boxfill%2Frainbow',
-          name: 'Black sea CHL',
+          name: 'Black sea CHL L4 Product',
           bounds: latLngBounds(latLng([40, 26.5]), latLng([48, 42])),
+        }, {
+          ...cmemsDisplay,
+          baseUrl: 'https://nrt.cmems-du.eu/thredds/wms/dataset-oc-bal-chl-olci_a-l3-nn_1km_daily-rt-v02?COLORSCALERANGE=0.03%2C30&STYLES=boxfill%2Frainbow',
+          name: 'Baltic sea CHL L3 Product',
+          visible: false,
+          bounds: latLngBounds(latLng([53.25, 9]), latLng([65.85, 30.5])),
         }],
       },
     },
