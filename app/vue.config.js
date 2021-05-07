3f6e8c9592140585059689ad6569e6a73da496f5
santilland <daniel.santillan@eox.at>
2021-05-07 11:55:42 +0200

    disabled prettify to see if there is a speed difference in build

--- app/vue.config.js
+++ app/vue.config.js
@@ -77,3 +77,17 @@ module.exports = {
     ],
   },
 };
+
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap((options) => {
+        options.prettify = false;
+        return options;
+      });
+  },
+};
