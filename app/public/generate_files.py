7e96ce1c076a5bf09c5d93bd46d51000b3dbde6c
santilland <daniel.santillan@eox.at>
2021-06-30 14:07:14 +0200

    updated to use geodb for e10a10

--- app/public/generate_files.py
+++ app/public/generate_files.py
@@ -425,7 +425,6 @@ generateData(
         '/working/data/trilateral/E10a6.csv',
         '/working/data/trilateral/E10a8.csv',
         '/working/eodash-data/data/E10a9.csv',
-        '/working/eodash-data/data/E10a10.csv',
         '/working/eodash-data/data/E13b2.csv',  # archived
         '/working/eodash-data/data/E13d_detections.csv',
         '/working/eodash-data/data/N1a_PM25_CAMS.csv',
@@ -453,6 +452,7 @@ generateData(
         ['E4', ''],
         ['E5', ''],
         ['E8', ''],
+        ['E10a10', ''],
         ['E11', ''],
         ['E12b', ''],
         ['E13b', ''],
