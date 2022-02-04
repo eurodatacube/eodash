083a2913128ca4c23de056bba63d5d3452e588f4
Moritz Riede <spectrachrome@tuta.io>
2022-02-04 11:27:44 +0100

    Extract landing page components to increase readability and encapsulation

---
+++ app/src/components/ThemesLandingPage/Hero.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hero">
+    <div class="overlay d-flex flex-column justify-center align-center">
+      <h2 class="text-h2 mb-5 white--text">You and Earth Observation</h2>
+      <h5 class="text-h5 mb-10 white--text">A Tri-Agency Dashboard by NASA, ESA, JAXA</h5>
+      <p class="mb-10 white--text">
+        <!-- eslint-disable vue/no-v-html -->
+        International collaboration among space agencies is central to the success of satellite Earth observations and data analysis. These partnerships foster more comprehensive measurements, robust datasets, and cost-effective missions.
+        <!-- eslint-enable -->
+      </p>
+      <v-btn
+        elevation="2"
+        color="white"
+        class="black--text"
+        x-large
+      >Start Exploring</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+};
+</script>
+
+<style lang="scss">
+  .hero {
+    background-image: url('../../assets/landing-page-bg.jpg');
+    background-color: #000;
+    height: calc(100vh - 104px);
+    width: 100vw;
+    margin-top: 76px;
+
+    .overlay {
+      background: #000B;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+
+      p {
+        max-width: 700px;
+      }
+    }
+  }
+</style>
---
+++ app/src/components/ThemesLandingPage/ThemeNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container class="lighten-5" fluid>
+    <v-row no-gutters>
+      <template v-for="(theme, i) in themes">
+        <v-col :key="theme.slug" class="px-1" cols="12" xs="12" sm="12" md="6" lg="2">
+          <v-btn
+            elevation="1"
+            :color="theme.color"
+            style="min-width: 100%; max-width: 100%; color: rgba(0, 0, 0, 0.9);"
+            large
+          >{{ theme.name }}</v-btn>
+        </v-col>
+      </template>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import {
+  mapState,
+} from 'vuex';
+
+export default {
+  props: {},
+  computed: {
+    ...mapState({ themes: state => state.themes.themes }),
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
