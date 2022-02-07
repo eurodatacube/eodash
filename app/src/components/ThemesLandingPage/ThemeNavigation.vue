0309a0ca078b82ab92b825445bbd4d8de975275e
Moritz Riede <spectrachrome@tuta.io>
2022-02-07 13:58:49 +0100

    Use Vuetify's expansion panel component for the mobile menu

--- app/src/components/ThemesLandingPage/ThemeNavigation.vue
+++ app/src/components/ThemesLandingPage/ThemeNavigation.vue
@@ -1,24 +1,31 @@
 <template>
   <v-container class="lighten-5" fluid>
     <div class="mobile-menu d-flex d-md-none">
-      <v-btn class="extender justify-center align-center" @click="isExtended = !isExtended">
-        Topics  v
-      </v-btn>
-      <v-row no-gutters class="button-row" style="z-index: 1000;" :class="{active: isExtended}">
-        <template v-for="theme in themes">
-          <v-col :key="theme.slug" class="px-1" cols="12" xs="12" sm="12" md="6" lg="2">
-            <v-btn
-              elevation="1"
-              class="mb-2 mb-lg-0 white--text"
-              :color="theme.color"
-              style="min-width: 100%; max-width: 100%;"
-              large
-              @click="$router.push('/themes/' + theme.slug)"
-            >{{ theme.name }}</v-btn>
-          </v-col>
-        </template>
-      </v-row>
+      <v-expansion-panels accordion>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            Topics
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-row no-gutters class="button-row" style="z-index: 1000;">
+              <template v-for="theme in themes">
+                <v-col :key="theme.slug" class="px-1" cols="12" xs="12" sm="12" md="6" lg="2">
+                  <v-btn
+                    elevation="1"
+                    class="mb-2 mb-lg-0 white--text"
+                    :color="theme.color"
+                    style="min-width: 100%; max-width: 100%;"
+                    large
+                    @click="$router.push('/themes/' + theme.slug)"
+                  >{{ theme.name }}</v-btn>
+                </v-col>
+              </template>
+            </v-row>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </div>
+
     <div class="normal-menu d-none d-md-flex">
       <v-row no-gutters class="button-row-2">
         <template v-for="theme in themes">
@@ -61,15 +68,5 @@ export default {
   width: 100vw;
   height: 30px;
   background: #777;
-}
-
-.button-row {
-  height: 0;
-  overflow: hidden;
-  transition: max-height 0.2s ease;
-
-  &.active {
-    height: 10000px;
-  }
 }
 </style>
