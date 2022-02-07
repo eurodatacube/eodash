d8d065f6ea4440099bf7b6ce8adac0ea0e3387bc
Moritz Riede <spectrachrome@tuta.io>
2022-02-07 10:32:53 +0100

    Use plain earth as background and make overlay position absolute

--- app/src/components/ThemesLandingPage/Hero.vue
+++ app/src/components/ThemesLandingPage/Hero.vue
@@ -4,22 +4,22 @@
       <!--<div class="hero-section d-flex flex-column justify-center align-center image">
         
       </div>-->
-      <div class="hero-section d-flex flex-column justify-center align-center">
-        <h2 class="text-h2 mb-5 white--text">You and Earth Observation</h2>
-        <!--<h5 class="text-h5 mb-10 white--text">A Tri-Agency Dashboard by NASA, ESA, JAXA</h5>-->
-        <h5 class="text-h5 mb-5 white--text"><img src="/data/trilateral/Trilateral_Logo.svg" height="40" /></h5>
-        <p class="mb-10 white--text">
-          <!-- eslint-disable vue/no-v-html -->
-          International collaboration among space agencies is central to the success of satellite Earth observations and data analysis. These partnerships foster more comprehensive measurements, robust datasets, and cost-effective missions.
-          <!-- eslint-enable -->
-        </p>
-        <v-btn
-          elevation="2"
-          color="white"
-          class="black--text"
-          x-large
-        >Start Exploring</v-btn>
-      </div>
+    </div>
+    <div class="hero-section d-flex flex-column justify-center align-center">
+      <h2 class="text-h2 mb-5 white--text">You and Earth Observation</h2>
+      <!--<h5 class="text-h5 mb-10 white--text">A Tri-Agency Dashboard by NASA, ESA, JAXA</h5>-->
+      <h5 class="text-h5 mb-5 white--text"><img src="/data/trilateral/Trilateral_Logo.svg" height="40" /></h5>
+      <p class="mb-10 white--text">
+        <!-- eslint-disable vue/no-v-html -->
+        International collaboration among space agencies is central to the success of satellite Earth observations and data analysis. These partnerships foster more comprehensive measurements, robust datasets, and cost-effective missions.
+        <!-- eslint-enable -->
+      </p>
+      <v-btn
+        elevation="2"
+        color="white"
+        class="black--text"
+        x-large
+      >Start Exploring</v-btn>
     </div>
   </div>
 </template>
@@ -32,14 +32,23 @@ export default {
 
 <style lang="scss">
   .hero {
-    //background-image: url('../../assets/landing-page-bg.jpg');
+    position: relative;
     height: 50vh;
     width: 100vw;
     margin-top: 76px;
+    background: #00040f;
 
     .hero-section {
+      position: absolute;
+      left: 0;
+      top: 0;
       width: 100%;
       height: 100%;
+      text-align: center;
+
+      p {
+        max-width: 700px;
+      }
 
       &.image {
         background-size: cover;
@@ -48,14 +57,16 @@ export default {
     }
 
     .overlay {
-      background: #00040f;
+      position: absolute;
+      left: 0;
+      top: 0;
+      
       width: 100%;
       height: 100%;
-      text-align: center;
-
-      p {
-        max-width: 700px;
-      }
+      background-size: cover;
+      background-position: center;
+      background-image: url('../../assets/earth_bg.jpg');
+      opacity: 0.3;
     }
   }
 </style>
