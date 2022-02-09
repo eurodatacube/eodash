8310b170cc79c15830a5e44373e528963ab9659d
Moritz Riede <spectrachrome@tuta.io>
2022-02-09 12:21:19 +0100

    Create header bar and extract theme button's styles into the CSS section

--- app/src/views/Topic.vue
+++ app/src/views/Topic.vue
@@ -18,14 +18,18 @@
           v-if="$vuetify.breakpoint.mdAndUp"
           class="text-uppercase mr-5"
         >
-          <span class="mr-3">{{ appConfig && appConfig.branding.appName }}</span>/<span class="ml-3" style="border-radius: 4px; text-transform: none; background: #FFF4; font-size: 90%; padding: 2px 5px;">{{ topic.name }}</span>
+          <span class="mr-3">{{ appConfig && appConfig.branding.appName }}</span>/<router-link class="ml-3 topic-button" :to="{name: topic.slug}">{{ topic.name }}</router-link>
         </v-toolbar-title>
         </router-link>
         <v-spacer></v-spacer>
         <img class="header__logo" :src="appConfig && appConfig.branding.headerLogo" />
       </v-app-bar>
       <v-row class="topic-page" justify="center">
-
+        <div class="topic-header d-flex justify-center align-center" :style="{background: topic.color}">
+          <div class="backdrop">
+          </div>
+          <h2 class="text-h2 white--text">{{ topic.name }}</h2>
+        </div>
       </v-row>
       <global-footer :color="topic.color" />
     </div>
@@ -77,10 +81,36 @@ export default {
 <style lang="scss" scoped>
 .topic-page {
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
 }
+.topic-header {
+  width: 100vw;
+  height: 30vh;
+  position: relative;
+}
+
+.topic-header .backdrop {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+
+.topic-button {
+  border-radius: 4px;
+  background: #FFF4;
+  text-transform: none;
+  font-size: 90%;
+  padding: 2px 5px;
+  text-decoration: none;
+  color: #FFF;
+}
+
 .section {
   min-height: 70vh;
-  min-width: 100vw;
+  width: 100vw;
 }
 </style>
