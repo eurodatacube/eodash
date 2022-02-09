2bf52bc59e75946715cb39f87b1558b208a89863
Moritz Riede <spectrachrome@tuta.io>
2022-02-09 16:07:37 +0100

    Move stories data into Vuex

--- app/src/store/modules/themes.js
+++ app/src/store/modules/themes.js
@@ -1,6 +1,9 @@
+import stories from '../../config/stories2.json'
+
 /* eslint no-shadow: ["error", { "allow": ["state"] }] */
 const state = {
   currentTheme: '',
+  stories: stories,
   themes: [
     {
       name: 'Atmospheric Composition',
@@ -41,8 +44,9 @@ const state = {
 };
 
 const getters = {
-  getThemes: (state)           => state.themes,
-  getTheme:  (state) => (slug) => state.themes.find(theme => theme.slug === slug),
+  getThemes:  (state)           => state.themes,
+  getTheme:   (state) => (slug) => state.themes.find(theme => theme.slug === slug),
+  getStories: (state)           => state.stories,
 };
 
 const mutations = {
