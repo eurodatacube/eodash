d4040241a82db9c08c3b241c0465a69758b594bb
Moritz Riede <spectrachrome@tuta.io>
2022-02-11 11:46:06 +0100

    Create new header component

---
+++ app/src/components/GlobalHeader.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app-bar
+    app
+    clipped-left
+    clipped-right
+    flat
+    color="primary"
+    class="global-header white--text"
+  >
+    <slot name="left"></slot>
+
+    <v-toolbar-title
+      v-if="$vuetify.breakpoint.mdAndUp"
+      class="text-uppercase mr-5 logo"
+      :class="{highlighted: $route.name === 'landing'}"
+    >
+      {{ appConfig && appConfig.branding.appName }}
+    </v-toolbar-title>
+    
+    <slot name="right"></slot>
+
+    <v-spacer></v-spacer>
+    <img class="header__logo" :src="appConfig && appConfig.branding.headerLogo" />
+  </v-app-bar>
+</template>
+
+<script>
+import {
+  mapState,
+  mapGetters,
+} from 'vuex';
+
+/**
+ * A global navbar component that adapts to different environments.
+ * @displayName GlobalHeader
+ */
+export default {
+  computed: {
+    ...mapState('config', [
+      'appConfig',
+    ]),
+    ...mapGetters({
+      currentTheme: 'themes/getCurrentTheme',
+    }),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.global-header {
+  .breadcrumb {
+    border-radius: 4px;
+    background: transparent;
+    text-transform: none;
+    padding: 0;
+    font-size: 90%;
+    text-decoration: none;
+    font-weight: normal;
+    color: #FFF;
+
+    &.logo {
+      font-size: 105%;
+    }
+
+    &.highlighted {
+      background: #FFF4;
+      padding: 3px 7px;
+    }
+  }
+}
+</style>
