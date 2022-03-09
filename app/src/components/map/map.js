78bf86471f5f36e1712d8bf8ebdae73f526f519a
RobertOrthofer <orthofer.robert@gmail.com>
2022-03-09 18:18:31 +0100

    simplify map instance creation

--- app/src/components/map/map.js
+++ app/src/components/map/map.js
@@ -7,38 +7,29 @@ import Zoom from 'ol/control/Zoom';
 import 'ol/ol.css';
 import './olControls.css';
 
-/**
- * creates the map instance, to be used as single vue instance property only.
- * @returns {Map}
- */
-function createMapInstance() {
-  const map = new Map({
-    controls: [
-      new FullScreen({
-        className: 'theme--dark v-card ol-full-screen',
-      }),
-      new Zoom({
-        className: 'theme--dark v-card ol-zoom',
-      }),
-      new Attribution({
-        className: 'theme--dark v-card ol-attribution',
-      }),
-    ],
-    view: new View({
-      zoom: 0,
-      center: [0, 0],
-      padding: [0, 0, 0, 0],
-      enableRotation: false,
-    }),
-  });
-
-  return map;
-}
-
 
 class VueMap {
   constructor() {
-    this.map = createMapInstance();
+    this.controls = [
+      new FullScreen({
+        className: 'v-card ol-full-screen',
+      }),
+      new Zoom({
+        className: 'v-card ol-zoom',
+      }),
+      new Attribution({
+        className: 'v-card ol-attribution',
+      }),
+    ];
+    this.map = new Map({
+      controls: this.controls,
+      view: new View({
+        zoom: 0,
+        center: [0, 0],
+        padding: [0, 0, 0, 0],
+        enableRotation: false,
+      }),
+    });
   }
 }
 const mapRegistry = {};
