2a83a5e4bf777a8661064254579bb966f5ba2e92
Moritz Riede <spectrachrome@tuta.io>
2022-03-17 16:26:48 +0100

    feat: create success card and move into separate component

--- app/src/components/Modal.vue
+++ app/src/components/Modal.vue
@@ -1,129 +1,136 @@
 <template>
-  <v-card
-    style="overflow-y: auto; height: 100%; max-width: 100% !important;">
-    <!--$vuetify.breakpoint.mdAndUp && 'px-10 py-4'-->
-    <v-form
-      ref="form"
-
-      lazy-validation
-      class="text-left"
-      @submit.prevent="submit"
+  <div>
+    <success-card
+      v-if="isFinished"
+      @close="close"
+    />
+    <v-card
+      style="overflow-y: auto; height: 100%; max-width: 100% !important;"
+      v-else
     >
-      <v-card-text class="text-center">
-        <template v-if="!alwaysSm">
-          <h1
-            class="display-2 font-weight-light primary--text mb-3"
-          >{{ titles[mode] }}</h1>
-          <h2
-            class="font-weight-light primary--text mb-4"
-          >{{ subtitles[mode] }}</h2>
-        </template>
+      <!--$vuetify.breakpoint.mdAndUp && 'px-10 py-4'-->
+      <v-form
+        ref="form"
+        lazy-validation
+        class="text-left"
+        @submit.prevent="submit"
+      >
+        <v-card-text class="text-center">
+          <template v-if="!alwaysSm">
+            <h1
+              class="display-2 font-weight-light primary--text mb-3"
+            >{{ titles[mode] }}</h1>
+            <h2
+              class="font-weight-light primary--text mb-4"
+            >{{ subtitles[mode] }}</h2>
+          </template>
 
-        <template v-else>
-          <h2
-            class="display-1 font-weight-light primary--text mb-3"
-          >{{ titles[mode] }}</h2>
-          <h3
-            class="font-weight-light primary--text mb-4"
-          >{{ subtitles[mode] }}</h3>
-        </template>
+          <template v-else>
+            <h2
+              class="display-1 font-weight-light primary--text mb-3"
+            >{{ titles[mode] }}</h2>
+            <h3
+              class="font-weight-light primary--text mb-4"
+            >{{ subtitles[mode] }}</h3>
+          </template>
 
-        <v-card outlined class="pa-3">
-            <v-row>
-              <v-col cols="12" class="pb-2 pt-4" v-if="mode === 'dashboard'">
-                <h2 class="mb-3">Dashboard Title</h2>
-                  <v-text-field
-                    v-model="popupTitle"
-                    hint="You will be able to change this later"
-                    persistent-hint
-                    :rules="form.title.rules"
-                    placeholder="Title"
-                    required
+          <v-card outlined class="pa-3">
+              <v-row>
+                <v-col cols="12" class="pb-2 pt-4" v-if="mode === 'dashboard'">
+                  <h2 class="mb-3">Dashboard Title</h2>
+                    <v-text-field
+                      v-model="popupTitle"
+                      hint="You will be able to change this later"
+                      persistent-hint
+                      :rules="form.title.rules"
+                      placeholder="Title"
+                      required
+                      outlined
+                      validate-on-blur
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12" class="pb-2 pt-0">
+                  <h2 class="my-3">Your interests</h2>
+                  <v-combobox
+                    v-model="form.interests.value"
+                    :items="form.interests.options"
+                    type="button"
+                    placeholder="Your interests"
                     outlined
-                    validate-on-blur
-                  ></v-text-field>
-              </v-col>
-              <v-col cols="12" class="pb-2 pt-0">
-                <h2 class="my-3">Your interests</h2>
-                <v-combobox
-                  v-model="form.interests.value"
-                  :items="form.interests.options"
-                  type="button"
-                  placeholder="Your interests"
-                  outlined
-                  multiple
-                  small-chips
-                  hint="This helps us provide better, personalized content to you"
-                  persistent-hint
-                  required
-                  :rules="form.interests.rules"
-                  validate-on-blur
-                ></v-combobox>
-              </v-col>
-              <v-col cols="12" class="pb-2 pt-0">
-                <h2 class="mb-3">Your name</h2>
-                  <v-text-field
-                    v-model="form.name.value"
-                    :rules="form.name.rules"
-                    placeholder="Name"
-                    required
-                    outlined></v-text-field>
-              </v-col>
-              <v-col cols="12" class="pb-2 pt-0">
-                <h2 class="mb-3">Your email address</h2>
-                  <v-text-field
-                    :hint="mode === 'newsletter'
-                      ? ''
-                      : 'You will receive your dashboard links to this address'
-                    "
+                    multiple
+                    small-chips
+                    hint="This helps us provide better, personalized content to you"
                     persistent-hint
-                    v-model="form.email.value"
-                    :rules="form.email.rules"
-                    placeholder="E-mail"
                     required
-                    outlined></v-text-field>
-              </v-col>
-              <v-col cols="12" class="pb-0 pt-0">
-                <v-checkbox
-                  v-model="form.privacyConsent.value"
-                  :rules="form.privacyConsent.rules"
-                  required>
-                  <template v-slot:label>
-                    I have read and accepted the
-                    <a @click.stop href='/privacy' target="_blank">
-                      Privacy Notice and Consent Form
-                    </a>
-                  </template>
-                </v-checkbox>
-              </v-col>
-              <!--<v-col cols="12" class="pb-2 pt-0" v-if="mode === 'dashboard'">
-                <v-checkbox
-                  v-model="newsletterOptIn">
-                  <template v-slot:label>
-                    I want to stay up-to-date about {{ appConfig
-                      && appConfig.branding.appName }} via newsletter
-                  </template>
-                </v-checkbox>
-              </v-col>-->
-          </v-row>
-        </v-card>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-          color="primary"
-          text
-          @click="close"
-          x-large
-        >Back</v-btn>
-        <v-btn
-          color="success"
-          type="submit"
-          x-large
-          :loading="isLoading">Submit</v-btn>
-      </v-card-actions>
-    </v-form>
-  </v-card>
+                    :rules="form.interests.rules"
+                    validate-on-blur
+                  ></v-combobox>
+                </v-col>
+                <v-col cols="12" class="pb-2 pt-0">
+                  <h2 class="mb-3">Your name</h2>
+                    <v-text-field
+                      v-model="form.name.value"
+                      :rules="form.name.rules"
+                      placeholder="Name"
+                      required
+                      outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" class="pb-2 pt-0">
+                  <h2 class="mb-3">Your email address</h2>
+                    <v-text-field
+                      :hint="mode === 'newsletter'
+                        ? ''
+                        : 'You will receive your dashboard links to this address'
+                      "
+                      persistent-hint
+                      v-model="form.email.value"
+                      :rules="form.email.rules"
+                      placeholder="E-mail"
+                      required
+                      outlined></v-text-field>
+                </v-col>
+                <v-col cols="12" class="pb-0 pt-0">
+                  <v-checkbox
+                    v-model="form.privacyConsent.value"
+                    :rules="form.privacyConsent.rules"
+                    required>
+                    <template v-slot:label>
+                      I have read and accepted the
+                      <a @click.stop href='/privacy' target="_blank">
+                        Privacy Notice and Consent Form
+                      </a>
+                    </template>
+                  </v-checkbox>
+                </v-col>
+                <!--<v-col cols="12" class="pb-2 pt-0" v-if="mode === 'dashboard'">
+                  <v-checkbox
+                    v-model="newsletterOptIn">
+                    <template v-slot:label>
+                      I want to stay up-to-date about {{ appConfig
+                        && appConfig.branding.appName }} via newsletter
+                    </template>
+                  </v-checkbox>
+                </v-col>-->
+            </v-row>
+          </v-card>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="close"
+            x-large
+          >Back</v-btn>
+          <v-btn
+            color="success"
+            type="submit"
+            x-large
+            :loading="isLoading">Submit</v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -131,18 +138,18 @@ import {
   mapActions,
 } from 'vuex';
 
+import SuccessCard from '@/components/Modal/SuccessCard.vue';
+
 export default {
+  components: {
+    SuccessCard,
+  },
   data: () => ({
+    isFinished: false,
     isLoading: false,
     isValid: false,
-    titles: {
-      dashboard: 'Save this dashboard',
-      newsletter: 'Subscribe to our newsletter',
-    },
-    subtitles: {
-      dashboard: 'Create a permanent link to your Dashboard configuration.',
-      newsletter: 'Get our latest platform updates delivered straight to your inbox.',
-    },
+    progress: 0.0,
+    
     form: {
       interests: {
         value: '',
@@ -195,6 +202,10 @@ export default {
       },
     },
   }),
+  mounted () {
+    this.progress = 0.0;
+    //this.isFinished = false;
+  },
   computed: {
     paddingClasses() {
       switch (this.$vuetify.breakpoint.name) {
@@ -242,11 +253,19 @@ export default {
       'addToMailingList',
     ]),
 
+    increaseProgress () {
+      this.progress = this.progress + 0.6;
+
+      if (this.progress >= 110.0) {
+        this.$emit('close');
+      }
+    },
+
     /**
      * Reset Vuetify form validation and signal our surroundings that we want to be closed.
      */
     close() {
-      this.$refs.form.resetValidation();
+      //this.$refs.form.resetValidation();
       this.$emit('close');
     },
 
@@ -277,7 +296,8 @@ export default {
             window.setTimeout(() => {
               this.isLoading = false;
               this.isNewsletterSubscribed = true;
-              this.$emit('submit');
+
+              this.isFinished = true;
             }, 200);
           })
           .catch((e) => console.log(`could not add to mailing list: ${e}`));
