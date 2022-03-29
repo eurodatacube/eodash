f4409fd2163cfd6e36609f584f7f1d8a1e1a1613
Robert Orthofer <orthofer.robert@gmail.com>
2022-03-29 17:38:59 +0200

    Mouse Position (#1343)
    * add mouse position control
    
    * disable pointer events for mouse position
    
    * consider multiple worlds

--- app/src/components/map/map.js
+++ app/src/components/map/map.js
@@ -3,6 +3,8 @@ import View from 'ol/View';
 import Attribution from 'ol/control/Attribution';
 import FullScreen from 'ol/control/FullScreen';
 import Zoom from 'ol/control/Zoom';
+import MousePosition from 'ol/control/MousePosition';
+import { toStringXY } from 'ol/coordinate';
 
 import 'ol/ol.css';
 import './olControls.css';
@@ -16,6 +18,20 @@ class VueMap {
       }),
       new Zoom({
         className: 'v-card primary--text ol-zoom',
+      }),
+      new MousePosition({
+        coordinateFormat: (coordinates) => {
+          let lonValue = coordinates[0] % 360;
+          if (lonValue > 180) {
+            lonValue -= 360;
+          } else if (lonValue < -180) {
+            lonValue += 360;
+          }
+          return `<span>${toStringXY([lonValue, coordinates[1]], 3)}</span>`;
+        },
+        projection: 'EPSG:4326',
+        className: 'v-card ol-control primary--text ol-mouse-position',
+        placeholder: false,
       }),
       new Attribution({
         className: 'v-card ol-attribution',
