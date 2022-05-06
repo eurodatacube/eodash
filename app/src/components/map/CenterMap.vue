9a1223b6e818c67e0dcab4230aa711354538c023
RobertOrthofer <orthofer.robert@gmail.com>
2022-05-06 11:51:19 +0200

    fix: reserve capital letters for class files

--- app/src/components/map/CenterMap.vue
+++ app/src/components/map/CenterMap.vue
@@ -23,7 +23,7 @@ import {
   mapState,
 } from 'vuex';
 import LayerControl from '@/components/map/LayerControl.vue';
-import getCluster from '@/components/map/Cluster';
+import getCluster from '@/components/map/cluster';
 import SpecialLayer from '@/components/map/SpecialLayer.vue';
 import getMapInstance from '@/components/map/map';
 import { formatLabel } from '@/components/map/formatters';
