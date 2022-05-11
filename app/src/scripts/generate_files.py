e85e45abaae87ae2d66f9b43d2829f5a730c9cf8
santilland <daniel.santillan@eox.at>
2022-05-11 12:51:36 +0200

    feat: added 2 new jaxa datasets

--- app/src/scripts/generate_files.py
+++ app/src/scripts/generate_files.py
@@ -142,6 +142,8 @@ ZARRCOLLECTIONS = [
 WMSCOLLECTIONS = {
     "ONPP-GCOMC-World-Monthly": "https://ogcpreview2.restecmap.com/examind/api/WS/wms/default?",
     "NDVI-GCOMC-World-Monthly": "https://ogcpreview2.restecmap.com/examind/api/WS/wms/default?",
+    "SMC-Anomaly-GCOMW-World-Monthly": "https://ogcpreview2.restecmap.com/examind/api/WS/wms/default?",
+    "PRC-Anomaly-GSMaP-World-Monthly": "https://ogcpreview2.restecmap.com/examind/api/WS/wms/default?",
 }
 
 STAC_COLLECTIONS = {
