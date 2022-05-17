72ff896ea948531c3812aa8be5d1d4d5268e8db1
RobertOrthofer <orthofer.robert@gmail.com>
2022-05-17 16:53:29 +0200

    fix: consider layer groups

--- app/src/components/map/timeLayerUtils.js
+++ app/src/components/map/timeLayerUtils.js
@@ -5,15 +5,27 @@
  * @param {*} config config object (e.g. "mergedConfigsData")
  * @param {*} timeObject time definition object
  */
+
+import LayerGroup from 'ol/layer/Group';
+import TileWMS from 'ol/source/TileWMS';
+
 // eslint-disable-next-line import/prefer-default-export
 export function updateTimeLayer(layer, config, time) {
-  const source = layer.getSource();
-  if (config.protocol === 'WMS') {
-    source.updateParams({
-      LAYERS: config.layers,
-      time: config.dateFormatFunction(time),
-      env: `year:${time}`,
-    });
+  let sources;
+  debugger;
+  if (layer instanceof LayerGroup) {
+    sources = layer.getLayers().getArray().map((l) => l.getSource());
+  } else {
+    sources = [layer.getSource()];
   }
-  source.refresh();
+  sources.forEach((source) => {
+    if (config.protocol === 'WMS' && source instanceof TileWMS) {
+      source.updateParams({
+        LAYERS: config.layers,
+        time: config.dateFormatFunction(time),
+        env: `year:${time}`,
+      });
+    }
+    source.refresh();
+  });
 }
