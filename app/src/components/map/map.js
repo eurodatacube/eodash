b8d8043161fe72ad7f785136bcaf7d3c6e3678dc
RobertOrthofer <orthofer.robert@gmail.com>
2022-08-16 07:53:23 +0200

    add maxZoom

--- app/src/components/map/map.js
+++ app/src/components/map/map.js
@@ -14,6 +14,7 @@ class VueMap {
         zoom: 0,
         center: [0, 0],
         padding: [20, 20, 20, 20],
+        maxZoom: 15,
         extent: options.constrainExtent,
         constrainOnlyCenter: true,
         enableRotation: false,
