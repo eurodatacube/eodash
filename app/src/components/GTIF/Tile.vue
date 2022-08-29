3079893331525236a22bc3caa8187b5bb51080b2
Moritz Riede <moritz@mriede.com>
2022-08-29 16:01:19 +0200

    feat: create new component which represents a single tile in the GTIF dashboard

---
+++ app/src/components/GTIF/Tile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="gtif-dashboard-tile d-flex rounded-lg bg-primary">
+
+  </div>
+</template>
+
+<script>
+// Utilities
+import {
+  mapState,
+} from 'vuex';
+
+export default {
+  name: 'GtifTile',
+  props: {
+    type: String,
+    required: true,
+  },
+  computed: {
+    ...mapState('config', [
+      'appConfig',
+    ]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.gtif-dashboard-tile {
+
+}
+</style>
