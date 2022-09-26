8f77d34e0c0c928b9925df0e0141cbe011869727
Moritz Riede <moritz@mriede.com>
2022-09-26 14:02:45 +0200

    feat: add explore to navigation menu items

--- app/src/components/GTIF/GTIFHeader.vue
+++ app/src/components/GTIF/GTIFHeader.vue
@@ -80,7 +80,17 @@
                   <svg class="mr-4 home-icon svg-inline--fa fa-home-lg-alt fa-w-18" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home-lg-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M288 115L69.47 307.71c-1.62 1.46-3.69 2.14-5.47 3.35V496a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16V368a16 16 0 0 1 16-16h96a16 16 0 0 1 16 16v128a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16V311.1c-1.7-1.16-3.72-1.82-5.26-3.2zm282.69 121.28l-255.94-226a39.85 39.85 0 0 0-53.45 0l-256 226a16 16 0 0 0-1.21 22.6L25.5 282.7a16 16 0 0 0 22.6 1.21L277.42 81.63a16 16 0 0 1 21.17 0L527.91 283.9a16 16 0 0 0 22.6-1.21l21.4-23.82a16 16 0 0 0-1.22-22.59z"></path></svg>
                 </div>
 
-                <div class="name">GTIF Home</div>
+                <div class="name">Home</div>
+              </v-row>
+
+              <v-row 
+                @click="$router.push({name: 'explore'})"
+                class="navrow py-5 px-7 fill-width" align="center"
+              >
+                <div class="w-12">
+                  <div style="background: hsl(215 80% 60%)" class="w-3 h-3 rounded-full dot mr-4"></div>            
+                </div>
+                <div class="name">Explore</div>
               </v-row>
 
               <v-row class="navrow py-5 px-7 fill-width" v-for="i in [0, 1, 2, 3, 4]" :key="i" align="center">
