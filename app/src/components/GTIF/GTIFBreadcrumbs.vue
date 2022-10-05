8362c27466da42fbdaaab008d4f8a8b81b8e393c
Moritz Riede <moritz@mriede.com>
2022-10-05 16:37:40 +0200

    feat: create a new component for the header row breadcrumbs

---
+++ app/src/components/GTIF/GTIFBreadcrumbs.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row
+    justify="between"
+    align="center"
+  >
+  </v-row>
+</template>
+
+<script>
+// Utilities
+import {
+  mapState,
+} from 'vuex';
+
+export default {
+  name: 'GtifHeaderNavItem',
+  computed: {
+    ...mapState('config', [
+      'appConfig',
+    ]),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.gtif-breadcrumbs {
+  background: #77F;
+}
+</style>
