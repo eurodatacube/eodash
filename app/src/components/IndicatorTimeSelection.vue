de4c904293ee911ec005e49501ece30bc161c879
silvester-pari <silvester.pari@eox.at>
2022-10-05 21:07:55 +0200

    fix: time selection menu positioning

--- app/src/components/IndicatorTimeSelection.vue
+++ app/src/components/IndicatorTimeSelection.vue
@@ -1,6 +1,7 @@
 <template>
   <v-sheet
     class="row justify-center align-center rounded"
+    :class="$vuetify.breakpoint.xsOnly && autofocus ? 'menu-top' : 'menu-bottom'"
     :style="`position: absolute; ${$vuetify.breakpoint.xsOnly && autofocus
       ? 'top: 10px'
       : 'bottom: 30px'}; z-index: 5; width: auto; max-width: 600px;`"
@@ -214,5 +215,8 @@ export default {
 <style scoped>
 ::v-deep .mdi-asterisk {
   visibility: hidden;
+}
+.menu-bottom ::v-deep .v-menu__content {
+  transform: translate(1%, -87%);
 }
 </style>
