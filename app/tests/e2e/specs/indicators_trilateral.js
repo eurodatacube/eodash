8daabf87f9d01a357a8e35d61c8100164f26618c
santilland <daniel.santillan@eox.at>
2022-11-22 10:12:17 +0100

    fix: fixed type in visit url

--- app/tests/e2e/specs/indicators_trilateral.js
+++ app/tests/e2e/specs/indicators_trilateral.js
@@ -10,7 +10,7 @@ describe('Indicator tests for trilateral', () => {
   it('test indicator chart', () => {
     // We can leave out the explore navigation as directly providing kvp changes to this mode
     // thus we can use test branch deployments
-    cy.visit('/&poi=CN02-N2');
+    cy.visit('/?poi=CN02-N2');
     cy.contains(' Shanghai: Partial Column (GOSAT FTS L1B V220.220) ').should('exist');
     cy.get('#bar-chart').should('exist');
   });
