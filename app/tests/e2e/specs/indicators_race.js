aae5f7e05d15c74ce5d234a7f5d84bf7688b112e
santilland <daniel.santillan@eox.at>
2022-11-22 17:18:51 +0100

    chore: creating tests dynamically for each different indicator from pois json file

--- app/tests/e2e/specs/indicators_race.js
+++ app/tests/e2e/specs/indicators_race.js
@@ -1,12 +1,21 @@
+import indicatorPOIs from '../../../public/data/internal/pois_eodash.json';
+
 describe('Indicator tests for race', () => {
   // Overwriting baseurl when localhost is defined changing it to load race instance
   if (Cypress.config().baseUrl.includes('localhost')) {
     const { baseUrl } = Cypress.config();
     Cypress.config('baseUrl', baseUrl.replace('localhost', '127.0.0.1'));
   }
-  it('test indicator chart', () => {
-    cy.visit('/?poi=EG1-E200');
-    cy.contains('Suez : Ships in Port').should('exist');
-    cy.get('#bar-chart').should('exist');
+  const testSamples = {};
+  indicatorPOIs.forEach((item) => {
+    if (!(item.indicator in testSamples)) {
+      testSamples[item.indicator] = item;
+    }
+  });
+  Object.entries(testSamples).forEach(([key, obj]) => {
+    it(`testing indicator ${key}`, () => {
+      cy.visit(`/?poi=${obj.aoiID}-${key}`);
+      cy.get('#bar-chart, #line-chart, #bubbleMap').should('exist');
+    });
   });
 });
--- app/tests/e2e/specs/indicators_trilateral.js
+++ app/tests/e2e/specs/indicators_trilateral.js
@@ -1,17 +1,31 @@
+import indicatorPOIs from '../../../public/data/internal/pois_trilateral.json';
+
 describe('Indicator tests for trilateral', () => {
-  /*
-  TODO: For indicator tests
-  * Create tests for all instances (race, trilateral, gtif)
-  * When exactly should tests be performed
-  * Create tests for map and chart component (image comparison?)
-  * Loop over all possible indicators?
-  * ...?
-  */
-  it('test indicator chart', () => {
-    // We can leave out the explore navigation as directly providing kvp changes to this mode
-    // thus we can use test branch deployments
-    cy.visit('/?poi=CN02-N2');
-    cy.contains(' Shanghai: Partial Column (GOSAT FTS L1B V220.220) ').should('exist');
-    cy.get('#bar-chart').should('exist');
+  // We generate an object where each indicator is contained once,
+  // only having information of one poi
+
+  // Currently pois configured as mappois in the appConfig fail
+  // TODO: Consider best way of loading the information from the appConfig, currently
+  // list just copied here
+  const configuredMapPois = [
+    'DE1-E13c', 'PL1-E13c', 'BE3-E13c', 'FR3-E13c', 'IT3-E13c',
+    'IT9-E13b', 'FR8-E13b', 'UK4-E13b', 'EG1-E13c', 'EG01-N1', 'EG01-N2',
+    'EG01-SIF',
+  ];
+  const testSamples = {};
+  indicatorPOIs.forEach((item) => {
+    console.log(`${item.aoiID}-${item.indicator}`);
+    if (!(item.indicator in testSamples)
+    && !configuredMapPois.includes(`${item.aoiID}-${item.indicator}`)) {
+      testSamples[item.indicator] = item;
+    }
+  });
+  Object.entries(testSamples).forEach(([key, obj]) => {
+    it(`testing indicator ${key}`, () => {
+      // We can leave out the explore navigation as directly providing kvp changes to this mode
+      // thus we can use test branch deployments
+      cy.visit(`/?poi=${obj.aoiID}-${key}`);
+      cy.get('#bar-chart, #line-chart, #bubbleMap').should('exist');
+    });
   });
 });
