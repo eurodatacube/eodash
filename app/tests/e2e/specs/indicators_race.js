ddd8085613ec510f4ae876123b34e5f66f143a90
santilland <daniel.santillan@eox.at>
2022-11-22 12:07:15 +0100

    feat: new approach to load correct domain when testing locally

--- app/tests/e2e/specs/indicators_race.js
+++ app/tests/e2e/specs/indicators_race.js
@@ -1,4 +1,9 @@
 describe('Indicator tests for race', () => {
+  // Overwriting baseurl when localhost is defined changing it to load race instance
+  if (Cypress.config().baseUrl.includes('localhost')) {
+    const { baseUrl } = Cypress.config();
+    Cypress.config('baseUrl', baseUrl.replace('localhost', '127.0.0.1'));
+  }
   it('test indicator chart', () => {
     cy.visit('/?poi=EG1-E200');
     cy.contains('Suez : Ships in Port').should('exist');
