01758773e6bf2e2a18f1e87755cf649df6ce3692
santilland <daniel.santillan@eox.at>
2022-11-30 14:59:42 +0100

    feat: added test runs to concurrency group, added resolution change to race tests

--- app/tests/e2e/specs/indicators_race.js
+++ app/tests/e2e/specs/indicators_race.js
@@ -1,21 +1,26 @@
 import indicatorPOIs from '../../../public/data/internal/pois_eodash.json';
 
 describe('Indicator tests for RACE', () => {
-  // Overwriting baseurl when localhost is defined changing it to load race instance
-  if (Cypress.config().baseUrl.includes('localhost')) {
-    const { baseUrl } = Cypress.config();
-    Cypress.config('baseUrl', baseUrl.replace('localhost', 'race.eox.world'));
-  }
-  const testSamples = {};
-  indicatorPOIs.forEach((item) => {
-    if (!(item.indicator in testSamples)) {
-      testSamples[item.indicator] = item;
+  context('1080p resolution', () => {
+    beforeEach(() => {
+      cy.viewport(1920, 1080);
+    });
+    // Overwriting baseurl when localhost is defined changing it to load race instance
+    if (Cypress.config().baseUrl.includes('localhost')) {
+      const { baseUrl } = Cypress.config();
+      Cypress.config('baseUrl', baseUrl.replace('localhost', 'race.eox.world'));
     }
-  });
-  Object.entries(testSamples).forEach(([key, obj]) => {
-    it(`testing indicator ${key}`, () => {
-      cy.visit(`/?poi=${obj.aoiID}-${key}`);
-      cy.get('#bar-chart, #line-chart, #bubbleMap').should('exist');
+    const testSamples = {};
+    indicatorPOIs.forEach((item) => {
+      if (!(item.indicator in testSamples)) {
+        testSamples[item.indicator] = item;
+      }
+    });
+    Object.entries(testSamples).forEach(([key, obj]) => {
+      it(`testing indicator ${key}`, () => {
+        cy.visit(`/?poi=${obj.aoiID}-${key}`);
+        cy.get('#bar-chart, #line-chart, #bubbleMap').should('exist');
+      });
     });
   });
 });
