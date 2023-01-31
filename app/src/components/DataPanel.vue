e05f30b9d5aad508968cef7168f55bf5f8f58b29
Lubomir Dolezal <lubojr@seznam.cz>
2023-01-31 11:52:35 +0100

    misc: remove reference to indicatorGlobe

--- app/src/components/DataPanel.vue
+++ app/src/components/DataPanel.vue
@@ -83,20 +83,8 @@
             >
               Regenerate
             </v-btn>
-            <indicator-globe
-              v-if="showGlobe"
-              @update:direction="d => direction = d"
-              @update:position="p => position = p"
-              @update:right="r => right = r"
-              @update:up="u => up = u"
-              @update:datalayertime="d => datalayertime = d"
-              @update:comparelayertime="c => comparelayertime = c"
-              @compareEnabled="compareEnabled = !compareEnabled"
-              class="d-flex justify-center"
-              style="top: 0px; position: absolute;"
-            />
             <template
-              v-else-if="customAreaIndicator && !customAreaIndicator.isEmpty"
+              v-if="customAreaIndicator && !customAreaIndicator.isEmpty"
             >
               <indicator-data
                 style="margin-top: 0px;"
@@ -366,7 +354,6 @@ import { loadIndicatorData } from '@/utils';
 import { createConfigFromIndicator } from '@/helpers/mapConfig';
 import { DateTime } from 'luxon';
 import IndicatorData from '@/components/IndicatorData.vue';
-import IndicatorGlobe from '@/components/IndicatorGlobe.vue';
 import IframeButton from '@/components/IframeButton.vue';
 import FilterControls from '@/components/map/FilterControls.vue';
 import StyleControls from '@/components/map/StyleControls.vue';
@@ -382,7 +369,6 @@ export default {
   ],
   components: {
     IndicatorData,
-    IndicatorGlobe,
     IframeButton,
     AddToDashboardButton,
     FilterControls,
@@ -529,9 +515,6 @@ export default {
     showMap() {
       // if returns true, we are showing map, if false we show chart
       return ['all'].includes(this.indicatorObject.country) || this.appConfig.configuredMapPois.includes(`${this.indicatorObject.aoiID}-${this.indicatorObject.indicator}`) || Array.isArray(this.indicatorObject.country);
-    },
-    showGlobe() {
-      return this.indicatorObject.showGlobe;
     },
     externalData() {
       const dataFromDefinition = this.baseConfig.indicatorsDefinition[
