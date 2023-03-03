9212db950efe8b397aa9e487af8e16b7dcf5b999
santilland <daniel.santillan@eox.at>
2023-03-03 11:06:28 +0100

    fix: updated to use hourly in mobility data

--- app/src/config/gtif.js
+++ app/src/config/gtif.js
@@ -1118,7 +1118,7 @@ export const globalIndicators = [
         lastColorCode: null,
         aoi: null,
         aoiID: 'AT',
-        time: getMinuteIntervals('2022-12-01T00:00:00Z', '2022-12-31T23:30:00Z', 30),
+        time: getMinuteIntervals('2022-12-01T00:00:00Z', '2022-12-07T22:00:00Z', 60),
         inputData: [''],
         yAxis: '',
         highlights: [
@@ -1141,7 +1141,7 @@ export const globalIndicators = [
           },
         ],
         queryParameters: {
-          sourceLayer: 'trajectories_on_edges_austria_december',
+          sourceLayer: 'trajectories_on_edges_austria_december_first_week',
           selected: 'congestion_index',
           dataInfo: 'AQ4',
           items: [
@@ -1199,7 +1199,7 @@ export const globalIndicators = [
           layerName: 'geodb_debd884d-92f9-4979-87b6-eadef1139394:GTIF_AT_Network_edges_3857',
           protocol: 'geoserverTileLayer',
           getColor: (feature, store, options) => {
-            let color = '#00000000';
+            let color = '#000000';
             const dataSource = options.dataProp ? options.dataProp : 'mapData';
             if (store.state.indicators.selectedIndicator
                 && store.state.indicators.selectedIndicator[dataSource]) {
@@ -1220,7 +1220,7 @@ export const globalIndicators = [
             }
             return color;
           },
-          id: 'trajectories_on_edges_austria_december',
+          id: 'trajectories_on_edges_austria_december_first_week',
           adminZoneKey: 'unique_id',
           parameters: 'unique_id,congestion_index,duration,speed,distance,n_trajectories,motorized_share',
           name: 'Human Mobility Patterns',
