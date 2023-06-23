577a3c96e49a81ff28e18e402084670d487f83d1
Moritz Riede <moritz@mriede.com>
2023-06-23 14:12:38 +0200

    feat: create a component which renders slider ticks using SVG

---
+++ app/src/components/map/SliderTicks.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="fill-width d-flex justify-center">
+    <svg
+      ref="svg"
+      style="width: 560px; height: 10px;"
+      :viewBox="`-1 0 ${svgWidth + 2} ${svgHeight}`"
+    >
+      <line
+        v-for="(line, index) in lines"
+        :key="index"
+        :x1="line"
+        :y1="svgHeight - 10"
+        :x2="line"
+        :y2="svgHeight"
+        stroke="black"
+      />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SvgComponent",
+  props: {
+    numLines: {
+      type: Number,
+      default: 20,
+    },
+  },
+  data() {
+    return {
+      svgWidth: 560,
+      svgHeight: 10,
+    };
+  },
+  computed: {
+    lines() {
+      let spacing = this.svgWidth / (this.numLines - 1);
+      return Array.from({ length: this.numLines }, (_, i) => i * spacing);
+    },
+  },
+  mounted() {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.svgWidth = this.$refs.svg.clientWidth;
+      this.svgHeight = this.$refs.svg.clientHeight;
+    },
+  },
+  watch: {
+    numLines() {
+      this.handleResize();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fill-width {
+  width: 100%;
+  height: 100%;
+}
+</style>
