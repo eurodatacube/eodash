7b277cd1715fc957fc3b08eb773a5623be8b586b
silvester-pari <silvester.pari@eox.at>
2023-08-23 14:34:00 +0000

    feat: gtif-specific style overrides

--- app/src/components/IndicatorFiltersPanel.vue
+++ app/src/components/IndicatorFiltersPanel.vue
@@ -1,7 +1,11 @@
 <template>
   <eox-itemfilter class="pa-2">
-    <h4 slot="filterstitle" style="margin-top: 8px">Filter</h4>
-    <h4 slot="resultstitle" style="margin-top: 8px">Indicators</h4>
+    <h4 slot="filterstitle" style="margin-top: 8px">
+      {{this.appConfig.id === "gtif" ? "Domains" : "Filter"}}
+    </h4>
+    <h4 slot="resultstitle" style="margin-top: 8px">
+      {{this.appConfig.id === "gtif" ? "Tools" : "Indicators"}}
+    </h4>
   </eox-itemfilter>
 </template>
 
@@ -107,13 +111,45 @@ export default {
           gtif: {
             titleProperty: 'title',
             filterProperties: [
-              { key: 'themes', title: 'Theme' },
+              {
+                key: 'themes', title: 'Theme', featured: true, type: 'select',
+              },
             ],
             onSelect: (item) => {
               this.setSelectedIndicator(item);
             },
             // exclusiveFilters: true,
             aggregateResults: 'themes',
+            styleOverride: `
+              #filters input[type=radio]:after,
+              #results input[type=radio]:after {
+                content: "";
+                background-size: cover;
+                background-position: center center;
+                border-radius: 50%;
+                width: 40px;
+                height: 40px;
+                margin: 6px;
+              }
+              #filters input[type=radio][id="energy transition"]:after {
+                background-image: url("https://gtif.esa.int/img/gtif/icons/energy-transition-trimmy.png");
+              }
+              #filters input[type=radio][id="mobility transition"]:after {
+                background-image: url("https://gtif.esa.int/img/gtif/icons/mobility-transition-trimmy.png");
+              }
+              #filters input[type=radio][id="sustainable cities"]:after {
+                background-image: url("https://gtif.esa.int/img/gtif/icons/sustainable-transition-trimmy.png");
+              }
+              #filters input[type=radio][id="carbon accounting"]:after {
+                background-image: url("https://gtif.esa.int/img/gtif/icons/carbon-finance-trimmy.png");
+              }
+              #filters input[type=radio][id="eo adaptation services"]:after {
+                background-image: url("https://gtif.esa.int/img/gtif/icons/eo-adaptation-trimmy.png");
+              }
+              #results input[type=radio]#item-0:after {
+                background-image: url("https://gtif.esa.int/data/gtif/globalDataLayerImages/AT-AQA.png");
+              }
+            `,
           },
         };
         this.itemfilter.config = configs[this.appConfig.id];
@@ -146,7 +182,10 @@ export default {
             }
           `;
         });
-        this.itemfilter.styleOverride = `${flags}`;
+        this.itemfilter.styleOverride = `
+          ${flags}
+          ${configs[this.appConfig.id].styleOverride}
+        `;
       });
     },
   },
