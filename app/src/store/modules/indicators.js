c6d6f7062b0ff6c11abe8a61d0d4ecbd47772fde
Lubomir Dolezal <lubojr@seznam.cz>
2023-10-16 15:41:54 +0200

    fix: themes not linked by comma

--- app/src/store/modules/indicators.js
+++ app/src/store/modules/indicators.js
@@ -57,7 +57,7 @@ const actions = {
         const indicators = [];
         data.links.forEach((link) => {
           if (link.rel === 'child') {
-            let themes = link.themes.split(',');
+            let themes = link.themes;
             if (themes && rootState.config.baseConfig.themeOverwrite) {
               const overwrite = rootState.config.baseConfig.themeOverwrite;
               const updated = [];
@@ -106,7 +106,7 @@ const actions = {
             // TODO: Replace as much configuration as possible by STAC information
             rootState.config.baseConfig.globalIndicators.forEach((indicator) => {
               if (indicator.properties.indicatorObject.indicator === resultIndicator.code) {
-                resultIndicator = { ...indicator.properties.indicatorObject, ...resultIndicator };
+                resultIndicator = { ...resultIndicator, ...indicator.properties.indicatorObject };
               }
             });
             indicators.push(resultIndicator);
