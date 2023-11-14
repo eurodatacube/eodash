035303f2affd6272acf424faf7351c79f1dbd76c
Moritz Riede <moritz@mriede.com>
2023-11-14 15:23:27 +0100

    chore: add minesweeper code

---
+++ app/src/plugins/minesweeper/board.js
@@ -0,0 +1,224 @@
+import { fromUrl } from 'geotiff';
+
+const EVEN_NEIGHBOR_OFFSETS = [[-1, -1], [0, -1], [-1, 0], [1, 0], [-1, 1], [0, 1]];
+const ODD_NEIGHBOR_OFFSETS = [[0, -1], [1, -1], [-1, 0], [1, 0], [0, 1], [1, 1]];
+
+/**
+ * Represents the game board and its possible states and calculations.
+ */
+export default class HexSweeperGame {
+  /**
+     * Create a new game board.
+     *
+     * @param {number} width - The width of the game board (in hex cells).
+     * @param {number} height - The height of the game board (in hex cells).
+     * @param {number} difficulty - The probability of a mine being in a cell.
+     */
+  constructor(width, height, difficulty) {
+    this.width = width;
+    this.height = height;
+    this.difficulty = difficulty;
+    this.board = [];
+  }
+
+  async fromGeoTIFF(url) {
+    try {
+      const tiff = await fromUrl(url);
+      const image = await tiff.getImage();
+      const data = (await image.readRasters({
+        width: this.width,
+        height: this.height,
+        resampleMethod: 'bilinear',
+      }))[0];
+
+      // Assuming the data is a single band and the size matches the game board
+      for (let y = 0; y < this.height; y++) {
+        const row = [];
+        for (let x = 0; x < this.width; x++) {
+          const value = data[y * this.width + x];
+
+          row.push({
+            isMine: value > 1500,
+            adjacentMines: 0,
+            isRevealed: false,
+            isFlagged: false,
+            element: null,
+          });
+        }
+
+        this.board.push(row);
+      }
+
+      for (let y = 0; y < this.height; y++) {
+        for (let x = 0; x < this.width; x++) {
+          const adjacentMines = this.calculateAdjacentMines(x, y);
+          this.board[y][x].adjacentMines = adjacentMines;
+        }
+      }
+
+      console.log(this.board);
+    } catch (error) {
+      console.error('Error loading GeoTIFF data:', error);
+    }
+  }
+
+  initializeBoard() {
+    for (let y = 0; y < this.height; y++) {
+      const row = [];
+      for (let x = 0; x < this.width; x++) {
+        row.push({
+          isMine: Math.random() < this.difficulty,
+          adjacentMines: 0,
+          isRevealed: false,
+          isFlagged: false,
+          element: null,
+        });
+      }
+      this.board.push(row);
+    }
+
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        this.board[y][x].adjacentMines = this.calculateAdjacentMines(x, y);
+      }
+    }
+  }
+
+  enforceBounds(x, y) {
+    if (this.isOutOfBounds(x, y)) {
+      console.warn(`Coordinates [${x}, ${y}] are out of bounds`);
+    }
+  }
+
+  getNeighborCoordinates(x, y) {
+    const offsets = (y % 2 === 0)
+      ? EVEN_NEIGHBOR_OFFSETS
+      : ODD_NEIGHBOR_OFFSETS;
+    return offsets.map(([dx, dy]) => [x + dx, y + dy]);
+  }
+
+  /**
+   * Convert axial coordinates to game board coordinates.
+   *
+   * @param {number} q - The axial column coordinate.
+   * @param {number} r - The axial row coordinate.
+   * @returns {{ x: number, y: number }} Game board coordinates.
+   */
+  convertAxialToGameCoords(q, r) {
+    const x = q + Math.floor(r / 2);
+    const y = r;
+    return { x, y };
+  }
+
+  /**
+   * Convert game board coordinates to axial coordinates.
+   *
+   * @param {number} x - The game board x coordinate.
+   * @param {number} y - The game board y coordinate.
+   * @returns {[number, number]} Axial coordinates [q, r].
+   */
+  convertGameCoordsToAxial(x, y) {
+    this.enforceBounds(x, y);
+    const q = x - Math.floor(y / 2);
+    const r = y;
+    return [q, r];
+  }
+
+  /**
+   * Gets the count of adjacent mines for a given tile on the game board.
+   *
+   * @param {number} x - The game board x coordinate.
+   * @param {number} y - The game board y coordinate.
+   * @returns {number} The count of adjacent mines.
+   */
+  getAdjacentMineCount(x, y) {
+    this.enforceBounds(x, y);
+    const tile = this.get(x, y);
+    return tile.adjacentMines;
+  }
+
+  /**
+     * Calculate the number of adjacent mines to a given cell.
+     *
+     * @param {number} x - The x-coordinate of the cell.
+     * @param {number} y - The y-coordinate of the cell.
+     *
+     * @returns {number} - The number of mines adjacent to the cell.
+     */
+  calculateAdjacentMines(x, y) {
+    let count = 0;
+
+    const neighbors = y % 2 === 0
+      ? EVEN_NEIGHBOR_OFFSETS
+      : ODD_NEIGHBOR_OFFSETS;
+
+    for (const [dx, dy] of neighbors) {
+      const nx = x + dx;
+      const ny = y + dy;
+
+      if (nx >= 0
+          && nx < this.width
+          && ny >= 0
+          && ny < this.height
+          && this.board[ny]
+          && this.board[ny][nx].isMine
+      ) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  isNextToMine(x, y) {
+    const neighbors = this.getNeighborCoordinates(x, y);
+    return neighbors.some(([nx, ny]) => this.isValidCoordinate(nx, ny)
+                                          && this.board[ny][nx].isMine);
+  }
+
+  revealTile(x, y) {
+    // Accumulate a list of revealed tile coordinates to update the rendering efficiently.
+    let coordinatePairs = [[x, y]];
+
+    const tile = this.get(x, y);
+    if (tile.isRevealed || tile.isFlagged) return [];
+    tile.isRevealed = true;
+
+      // Check if the tile is a non-mine with adjacent mines, then stop recursion.
+    if (tile.adjacentMines > 0 && !tile.isMine) {
+      return coordinatePairs;
+    }
+
+    const neighbors = this.getNeighborCoordinates(x, y);
+    neighbors.forEach(([nx, ny]) => {
+      if (this.isValidCoordinate(nx, ny) && !this.board[ny][nx].isRevealed) {
+        // Recursively reveal only if the neighbor is not a mine and has zero adjacent mines.
+        if (!this.board[ny][nx].isMine && this.board[ny][nx].adjacentMines === 0) {
+          coordinatePairs = coordinatePairs.concat(this.revealTile(nx, ny));
+        } else if (!this.board[ny][nx].isMine) {
+          // If the neighbor is not a mine but has adjacent mines, reveal it but do not recurse further.
+          this.board[ny][nx].isRevealed = true;
+          coordinatePairs.push([nx, ny]);
+        }
+      }
+    });
+
+    return coordinatePairs;
+  }
+
+  isValidCoordinate(x, y) {
+    return !!(this.board[y] && this.board[y][x]);
+  }
+
+  isOutOfBounds(x, y) {
+    return !this.isValidCoordinate(x, y);
+  }
+
+  isMine(x, y) {
+    return this.board[y] && this.board[y][x] && this.board[y][x].isMine;
+  }
+
+  get(x, y) {
+    this.enforceBounds(x, y);
+    return this.board[y][x];
+  }
+}
---
+++ app/src/plugins/minesweeper/index.js
@@ -0,0 +1,197 @@
+import { Feature } from 'ol';
+import { Polygon } from 'ol/geom';
+import { Image, Vector as VectorLayer } from 'ol/layer';
+import { Vector as VectorSource } from 'ol/source';
+import {
+  Text, Style, Fill, Stroke,
+} from 'ol/style';
+import HexGrid from 'ol-ext/render/HexGrid';
+import HexMap from 'ol-games/source/HexMap';
+
+import HexSweeperGame from './board';
+
+/**
+ * Set up the game grid using HexGrid and HexMap.
+ *
+ * @param {Object} map - The OpenLayers map instance.
+ * @returns {Object} The created `HexGrid`.
+ */
+const setupGrid = (map) => {
+  const size = 4000;
+  const boardCenter = [1800595.69763, 6140325.34559]; // Vienna
+
+  const grid = new HexGrid({
+    size,
+    origin: boardCenter,
+  });
+
+  const hex = new HexMap({ hexGrid: grid });
+  map.addLayer(new Image({ source: hex }));
+
+  return grid;
+};
+
+const updateTileVisuals = (x, y, grid, vectorSource, game) => {
+  const [q, r] = game.convertGameCoordsToAxial(x + 1, y);
+  const hexagonVertices = grid.getHexagon([q, r]);
+  const feature = new Feature(new Polygon([hexagonVertices]));
+  const tile = game.get(x, y);
+
+  console.log(`Tile: ${tile.isMine ? '💣' : tile.adjacentMines}`);
+
+  // Assign a unique identifier to the feature
+  const featureId = `tile-${x}-${y}`;
+  feature.setId(featureId);
+
+  // Remove the existing feature if it already exists
+  const existingFeature = vectorSource.getFeatureById(featureId);
+  if (existingFeature) {
+    vectorSource.removeFeature(existingFeature);
+  }
+  // Apply tile styling for mines, count, unexplored and flagged tiles.
+  feature.setStyle(getTileStyle(tile));
+  vectorSource.addFeature(feature);
+};
+
+const updateAllTileVisuals = (game, grid, vectorSource) => {
+  for (let y = 0; y < game.height; y++) {
+    for (let x = 0; x < game.width; x++) {
+      updateTileVisuals(x, y, grid, vectorSource, game);
+    }
+  }
+};
+
+/**
+* Handle a map click event.
+*
+* @param {Event} e - The click event.
+* @param {Object} map - The OpenLayers map instance.
+* @param {HexSweeperGame} game - The game instance.
+* @param {Object} grid - The hex grid.
+*/
+const handleMapClick = (e, game, grid, vectorSource) => {
+  e.stopPropagation();
+  e.preventDefault();
+
+  const { coordinate } = e;
+  console.log(coordinate);
+  // Get the axial coordinates of the clicked hexagon
+  const [q, r] = grid.coord2hex(coordinate);
+  const gameCoords = game.convertAxialToGameCoords(q, r);
+  const [x, y] = [gameCoords.x - 1, gameCoords.y];
+
+  console.log(`Got click: ${x}, ${y}`);
+
+  const revealedCoordsList = game.revealTile(x, y);
+
+  console.log(revealedCoordsList);
+
+  for (const [x, y] of revealedCoordsList) {
+    console.log('updating tile visuals');
+    updateTileVisuals(x, y, grid, vectorSource, game);
+  }
+};
+
+const getTileStyle = (tile) => {
+  let style;
+  console.log(tile);
+  if (tile.isRevealed === true) {
+    if (tile.isMine) {
+      style = new Style({
+        fill: new Fill({ color: 'red' }),
+        text: new Text({
+          text: '💣',
+          font: '20px Calibri,sans-serif',
+          fill: new Fill({ color: '#fff' }),
+          stroke: new Stroke({ color: '#000', width: 3 }),
+        }),
+      });
+    } else {
+      style = new Style({
+        fill: new Fill({ color: '#777' }),
+        text: new Text({
+          text: tile.adjacentMines ? tile.adjacentMines.toString() : '0',
+          font: '20px Calibri,sans-serif',
+          fill: new Fill({ color: '#000' }),
+          stroke: new Stroke({ color: '#fff', width: 3 }),
+        }),
+      });
+    }
+  } else if (tile.isFlagged) {
+    style = new Style({
+      fill: new Fill({ color: 'blue' }),
+      text: new Text({
+        text: '⚑',
+        font: '20px Calibri,sans-serif',
+        fill: new Fill({ color: '#fff' }),
+        stroke: new Stroke({ color: '#000', width: 3 }),
+      }),
+    });
+  } else {
+    style = new Style({
+      fill: new Fill({ color: '#aaa' }), // Unrevealed tile color
+      text: new Text({
+        text: '',
+        font: '20px Calibri,sans-serif',
+      }),
+    });
+  }
+
+  return style;
+};
+
+/**
+* Draw the game board by creating hexagon features and adding them to the map.
+*
+* @param {HexSweeperGame} game - The game instance.
+* @param {Object} grid - The hex grid.
+* @param {Object} map - The OpenLayers map instance.
+*/
+const drawGameBoard = (map, game, grid, vectorSource) => {
+  for (let y = 0; y < game.height; y++) {
+    // Make our edges straight again so the cell calculation works out
+    let xOffset = ((y % 2 !== 0) * 1) - y / 2;
+    if (y % 2 === 0) {
+      xOffset += 1.0;
+    } else {
+      xOffset += 0.5;
+    }
+
+    for (let x = 0; x < game.width; x++) {
+      const tile = game.board[y][x];
+      const hexCoords = grid.getHexagon([x + xOffset, y]);
+      const feature = new Feature(new Polygon([hexCoords]));
+
+      const style = getTileStyle(tile);
+      feature.setStyle(style);
+      vectorSource.addFeature(feature);
+    }
+  }
+
+  const vectorLayer = new VectorLayer({
+    source: vectorSource,
+  });
+
+  map.addLayer(vectorLayer);
+};
+
+/**
+* Initializes and sets up the hex map game.
+*
+* @param {Object} map - The OpenLayers map instance.
+*/
+export const createHexMap = async (map) => {
+  const grid = setupGrid(map);
+  const vectorSource = new VectorSource();
+  const game = new HexSweeperGame(40, 40, 0.2);
+
+  // game.initializeBoard();
+  await game.fromGeoTIFF('https://eox-gtif-public.s3.eu-central-1.amazonaws.com/ideas_data/Copernicus_DSM_30_N47_00_E014_00_DEM_COG.tif');
+  map.on('click', (e) => handleMapClick(e, game, grid, vectorSource));
+  drawGameBoard(map, game, grid, vectorSource);
+  updateAllTileVisuals(game, grid, vectorSource);
+};
+
+export default {
+  createHexMap,
+};
