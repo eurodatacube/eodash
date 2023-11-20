d7763607efa7005cb023ad63a3259c106cce8c0d
Lubomir Dolezal <lubojr@seznam.cz>
2023-11-20 12:11:33 +0100

    chore: gtif darker panel header

--- app/src/components/UiPanel.vue
+++ app/src/components/UiPanel.vue
@@ -7,7 +7,8 @@
     `"
   >
       <v-expansion-panel v-if="hasPanel">
-        <v-expansion-panel-header ref="header">
+        <v-expansion-panel-header ref="header"
+        :style="`background: ${gtif ? '#e9e9e9' : 'white'}`">
           {{ titleLabel }}
         </v-expansion-panel-header>
         <v-expansion-panel-content ref="expantionContent" eager
