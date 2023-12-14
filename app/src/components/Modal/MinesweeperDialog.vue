b4981c68dd37d24f37dba331a13c9a246a5b1ec0
Moritz Riede <moritz@mriede.com>
2023-12-14 11:52:19 +0100

    feat: start building new minesweeper modals and their stats

--- app/src/components/Modal/MinesweeperDialog.vue
+++ app/src/components/Modal/MinesweeperDialog.vue
@@ -1,15 +1,62 @@
 <template>
   <div class="modal-success">
-    <div class="d-flex flex-column align-center justify-center py-16">
-      <v-icon size="64" color="green lighten-4">💣</v-icon>
-      <h2 class="white--text my-4">Welcome to Minesweeper</h2>
+    <v-dialog v-model="isEnabled" width="500">
+      <!-- Activator Slot -->
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on">
+          <span>
+            💣
+          </span>
+        </v-btn>
+      </template>
 
-      <p>
-        Try to uncover all fields while carefully avoiding mines and learn about 
-        Earth Observation data along the way.
-      </p>
+      <!-- Default Slot for Dialog Content -->
+      <v-card>
+        <v-card-title style="text-align: center" class="py-6">💣 Minesweeper Game</v-card-title>
+        <v-card-text>
+          Try to uncover all fields while carefully avoiding mines and learn about 
+          Earth Observation data along the way. The amount of uncovered area at the
+          end of the game determines your score!
+        </v-card-text>
 
-      <v-btn class="mt-4 green darken-3 white--text" @click="$emit('close')">Dismiss</v-btn>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="green white--text" text @click="close()">Start Game</v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <v-card>
+        <v-card-title style="text-align: center" class="py-6">Game Over</v-card-title>
+        <v-card-text>
+          You stepped on a mine!
+
+          <div class="game-stats">
+            <div class="item">
+              <span class="name">🌟 TOTAL UNCOVERED AREA</span>
+              <span class="value">37%</span>
+            </div>
+
+            <div class="item">
+              <span class="name">⬡ NUMBER OF CELLS</span>
+              <span class="value">800</span>
+            </div>
+
+            <div class="item">
+              <span class="name">💣 NUMBER OF MINES</span>
+              <span class="value">276</span>
+            </div>
+          </div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="primary" text @click="close()">Continue</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <div v-if="mode === 'start'" class="d-flex flex-column align-center justify-center py-16">
+      <!-- ... -->
     </div>
   </div>
 </template>
@@ -25,6 +72,10 @@ export default {
     mode: {
       type: String,
       default: 'start',
+    },
+    isEnabled: {
+      type: Boolean,
+      default: false,
     },
   },
   methods: {
@@ -55,6 +106,32 @@ export default {
   top: 0;
   right: 0;
   bottom: 0;
+}
+
+.game-stats {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin: 20px 0;
+
+  .item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 16px;
+
+    .name {
+      font-weight: 500;
+      font-size: 16px;
+    }
+
+    .value {
+      font-size: 21px;
+      font-weight: 600;
+      color: #000;
+    }
+  }
 }
 
 .fullwidth {
