45054caba03bdfe44450f03102c771104d439b55
Moritz Riede <moritz@mriede.com>
2024-04-24 11:29:12 +0200

    fix: revert back to longer text

--- app/src/appConfig.js
+++ app/src/appConfig.js
@@ -55,7 +55,7 @@ module.exports = [
       '#6ce', '#994499', '#bbb', '#6633cc', '#e67300',
     ],
     uiText: {
-      indicatorPanelheader: 'Explore indicators',
+      indicatorPanelheader: 'Choose an indicator to explore',
     },
     demoMode: {
       egu2023: [
@@ -193,7 +193,7 @@ module.exports = [
       }
     },
     uiText: {
-      indicatorPanelheader: 'Explore indicators',
+      indicatorPanelheader: 'Choose an indicator to explore',
     },
     demoMode: {
       cop28: [
