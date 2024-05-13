6e309adcb5114e67d0f9ac16b098c619171c0eba
Daniel Santillan <daniel.santillan@eox.at>
2024-05-13 09:25:24 +0200

    chore: trying out exporting map config state in iframe text

--- app/src/components/IframeButton.vue
+++ app/src/components/IframeButton.vue
@@ -107,6 +107,9 @@ export default {
       'appConfig',
     ]),
     iframeCode() {
+      const mapConfig = JSON.stringify(this.$parent.$refs.mapContainer.config);
+      return mapConfig;
+      /*
       let indObj = this.indicatorObject;
       if (this.featureObject) {
         // Merge with parent indicator object to have all necessary information
@@ -124,6 +127,7 @@ export default {
         }
       }
       return `<iframe class="item" src="${window.location.origin}/iframe?${queryParams}" width="800px" height="500px" frameBorder="0" scroll="no" style="overflow:hidden"></iframe>`;
+      */
     },
   },
   methods: {
