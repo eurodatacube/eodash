d7e657ecb66d428035cfab6593b366ae1f653434
Daniel Santillan <daniel.santillan@eox.at>
2024-06-12 12:32:50 +0200

    chore [WIP] testing init method for eox story widgets to register projection

--- app/src/views/StoryEditor.vue
+++ app/src/views/StoryEditor.vue
@@ -10,6 +10,7 @@
         show-editor="open"
         markdown="## Welcome to the story editor "
         style="overflow-y: auto; height: calc(var(--vh, 2vh) * 100);"
+        @init="initWidgets"
       ></eox-storytelling>
       <global-footer />
     </div>
@@ -24,6 +25,23 @@ export default {
   components: {
     // GlobalHeader,
     GlobalFooter,
+  },
+  methods: {
+    initWidgets(element) {
+      // debugger;
+      if (element?.tagName === 'EOX-MAP') {
+        element.registerProjection(
+          'ESRI:53009',
+          '+proj=moll +lon_0=0 +x_0=0 +y_0=0 +a=6371000 '
+            + '+b=6371000 +units=m +no_defs',
+        );
+        const projection = element.getAttribute('projection');
+        if (projection) {
+          console.log('SETTING PROJECTION!!!!!');
+          element.setAttribute('projection', projection);
+        }
+      }
+    },
   },
 };
 </script>
