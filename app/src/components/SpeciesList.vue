677e9f1650b15e45a8ec3e01369fb3ff4396cedb
Moritz Riede <moritz@mriede.com>
2024-07-03 17:26:40 +0200

    feat: integrate species images

--- app/src/components/SpeciesList.vue
+++ app/src/components/SpeciesList.vue
@@ -1,8 +1,19 @@
 <template>
   <dl>
-    <div v-for="s in species" :key="s.species" style="margin-bottom: 16px;">
-      <dt><b>{{ s.species }}</b><span v-if="s.count > 1" class="count">{{ s.count }}</span></dt>
-      <dd v-if="s.common_name !== 'Unknown'">({{ s.common_name }})</dd>
+    <div
+      class="species v-row"
+      v-for="s in species"
+      :key="s.species"
+      style="margin-bottom: 16px;"
+    >
+      <img :src="s.image_url" />
+
+      <a :href="s.image_url" class="names" target="_blank">
+        <dt><b>{{ s.species }}</b></dt>
+        <dd v-if="s.common_name !== 'Unknown'">({{ s.common_name }})</dd>
+      </a>
+
+      <span v-if="s.count > 1" class="count">{{ s.count }}</span>
     </div>
   </dl>
 </template>
@@ -19,6 +30,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.species {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 16px;
+  min-width: 280px;
+
+  img {
+    height: 64px;
+    width: 64px;
+    border-radius: 4px;
+    margin-right: 12px;
+    border: 2px solid #99a;
+  }
+
+  .names {
+    width: 200px;
+  }
+}
+
 .count {
   height:22px;
   padding: 0 9px;
